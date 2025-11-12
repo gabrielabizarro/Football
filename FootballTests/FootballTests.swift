5313614eb515fa8edac0b129e4b088eea7b7ace9
gabrielabizarro <gbizarro2027@gmail.com>
2025-11-12 13:04:22 -0800

    Initial Commit

---
+++ FootballTests/FootballTests.swift
@@ -0,0 +1,16 @@
+//
+//  FootballTests.swift
+//  FootballTests
+//
+//  Created by BIZARRO, GABRIELA on 11/3/25.
+//
+
+import Testing
+
+struct FootballTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
